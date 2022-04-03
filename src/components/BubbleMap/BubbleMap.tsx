15569eaa7276b189e56fb8d388fde4a2cd4e47ff
Lucas Vogel <vogelino@users.noreply.github.com>
2022-04-03 17:30:43 +0200

    Extract bubbles into own component

--- src/components/BubbleMap/BubbleMap.tsx
+++ src/components/BubbleMap/BubbleMap.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react'
 import { LandMarks } from './LandMarks'
 import { useProjection } from '../../hooks/useProjection'
 import { useWorldMapLandMarksData } from '../../hooks/useWorldMapLandMarksData'
-import { scaleSqrt, max } from 'd3'
+import { scaleSqrt, max, ScaleLinear } from 'd3'
 import './BubbleMap.css'
 
 interface DataItem {
@@ -23,6 +23,29 @@ interface BubbleMapProps {
 const sizeValue = (d: { value: number }) => d.value
 const maxRadius = 20
 
+type BubblesProps = Pick<BubbleMapProps, 'data'> & {
+  projection: ReturnType<typeof useProjection>
+  sizeScale: ScaleLinear<number, number, never>
+}
+
+const Bubbles = ({ data, projection, sizeScale }: BubblesProps) => (
+  <>
+    {data.map(({ id, value, latitude, longitude }) => {
+      if (!projection) return null
+      const [x, y] = projection.projection([latitude, longitude]) || []
+      return (
+        <circle
+          className="data-circle"
+          key={id}
+          cx={x}
+          cy={y}
+          r={sizeScale(value)}
+        />
+      )
+    })}
+  </>
+)
+
 export const BubbleMap = ({
   data,
   filteredData,
@@ -42,18 +65,11 @@ export const BubbleMap = ({
   return (
     <g className="bubble-map">
       <LandMarks projection={projection} data={landMarksData} />
-      {filteredData.map(({ id, value, latitude, longitude }) => {
-        const [x, y] = projection.projection([latitude, longitude]) || []
-        return (
-          <circle
-            className="data-circle"
-            key={id}
-            cx={x}
-            cy={y}
-            r={sizeScale(value)}
-          />
-        )
-      })}
+      <Bubbles
+        sizeScale={sizeScale}
+        projection={projection}
+        data={filteredData}
+      />
     </g>
   )
 }
